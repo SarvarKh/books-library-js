ecc0ccc03306167e52af2d48924c0b19806395b0
Sarvar Khalimov <khalimovsarvar@gmail.com>
2021-05-13 15:54:28 +0500

    Refactor factory function to class function to create obj

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,15 @@ const book = (author, title, pages, read) => ({
   author, title, pages, read,
 });
 
+class Book {
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+  }
+}
+
 const addBookToLibrary = (newBook) => {
   myLibrary.push(newBook);
 };
